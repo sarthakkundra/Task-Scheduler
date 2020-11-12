27baeda9800d938a5409a8cdf17a255fa9e82f35
sarthakkundra <sarthakkundra21@gmail.com>
2020-11-12 17:53:45 +0530

    setup GraphQL

--- src/components/schedule/ScheduleCards.js
+++ src/components/schedule/ScheduleCards.js
@@ -1,19 +1,53 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from "react";
 
-import ScheduleCard from './ScheduleCard';
-import  res from '../../dummyData';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+import ScheduleCard from "./ScheduleCard";
+import res from "../../dummyData";
+
+const FEED_QUERY = gql`
+	{
+		task {
+			name
+			date
+			start_time
+			end_time
+		}
+	}
+`;
+
+var cards = res.data.task;
 
 const ScheduleCards = () => {
-    return (
-        <Fragment>
-            <div>
-            {res.data.task.map((task) => (
-            <ScheduleCard date={task.date}  name={task.name} start={task.start_time} end={task.end_time} />
-            )
-)}
-            </div>
-        </Fragment>
-    )
-}
+	return (
+		<Fragment>
+			<div>
+                    <Query query={FEED_QUERY}>
+                        {({  loading, error, data }) => {
+                            if(loading) return <div>Fetching</div>
+                            if(error) return <div>Error  Loading</div>
+                            console.log(data)
+                            cards = data.task
 
-export default ScheduleCards
+                            return (
+                                <div>
+                                {cards.map((task) => (
+                                    <ScheduleCard
+                                        date={task.date}
+                                        name={task.name}
+                                        start={task.start_time}
+                                        end={task.end_time}
+                                    />
+                                ))}
+                                </div>
+                            )
+                        }}
+				
+                    </Query>
+			</div>
+		</Fragment>
+	);
+};
+
+export default ScheduleCards;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { ApolloProvider } from 'react-apollo';
+import {  ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+  uri: `https://absolute-locust-15.hasura.app/v1/graphql`
+})
+
+const client  = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+})
 
 ReactDOM.render(
+ 
   <React.StrictMode>
+     <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
+ 
   document.getElementById('root')
 );
 
